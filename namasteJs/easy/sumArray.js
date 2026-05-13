4fb568b65d6d2afde5e1d4c83972e2201bce9e9c
suresh <sureshkumarbe04@gmail.com>
2026-05-13 12:31:45 +0530

    easy - sum array

--- namasteJs/easy/sumArray.js
+++ namasteJs/easy/sumArray.js
@@ -1,9 +1,30 @@
-console.log("sumArray...");
 function sumArray(arr) {
   function helper(i) {
     // your solution here
+
+    return i.reduce((acc, cur) => {
+      return cur + acc;
+    }, 0);
   }
-  return helper(0);
+  return helper(arr);
 }
 
-module.exports = { sumArray };
+const sampleData = {
+  sampleData1: [1, 2, 3],
+  sampleData2: [5],
+  empty: [],
+  withNegative1: [-1, 5, -4],
+  withNegative2: [1, -4],
+  withDecimal1: [1.5, 2.3, 3],
+  withDecimal2: [-1.2, 2.2],
+  largeSet1: [1, 20, 40, 3, 50, 60],
+};
+
+console.log(
+  `sampleData: `,
+  Object.keys(sampleData).map((k) => {
+    return `${k} is ${sumArray(sampleData[k])}`;
+  })
+);
+
+// module.exports = { sumArray };
