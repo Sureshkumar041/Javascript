5793f5d5404bb542bf684615d3a537470014092e
suresh <sureshkumarbe04@gmail.com>
2026-05-15 10:03:08 +0530

    easy - find max largest

--- namasteJs/easy/findMaxNumber.js
+++ namasteJs/easy/findMaxNumber.js
@@ -1,14 +1,30 @@
 function findMaxNumber(arr) {
-  // Your implementation
+  if (!arr.length) return null;
 
-  console.log("Arr: ", arr);
-  let maxNum = null;
-
-  if(!arr.length) return maxNum;
-
-//   for(let i = 0; i )
-
+  return arr.reduce((acc, cur) => {
+    return cur > acc ? cur : acc;
+  }, arr[0]);
 }
 
-//For the purpose of user debugging.
+const testingData = {
+  empty: [],
+  singleEven: [2],
+  singleOdd: [1],
+  mixedArr1: [1, 111, 4, 3, -2],
+  mixedArr2: [1, 2, 3, 4],
+  even: [2, 4, 6],
+  noEven: [1, 3, 5],
+  zero1: [0],
+  zero2: [0, 0, 0],
+  negativeVal: [-1, -2, -100],
+  withZero: [0, 2, 5],
+};
+
 findMaxNumber([1, 2, 3, 4, 5]);
+
+console.log(
+  `Output: `,
+  Object.keys(testingData).map((k) => {
+    return { [`${k} - ${testingData[k]}`]: findMaxNumber(testingData[k]) };
+  })
+);
