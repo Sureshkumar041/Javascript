5e690089261a88afe9e3ac5a6c1574eb9f28c748
suresh <sureshkumarbe04@gmail.com>
2026-05-21 11:11:10 +0530

    reserve words

--- namasteJs/easy/reverseWords.js
+++ namasteJs/easy/reverseWords.js
@@ -1,11 +1,10 @@
 function reverseWords(sentence) {
-  let reversingWord = "";
-
-  for (let i = 0; i < String(sentence).length; i++) {
-    reversingWord = reversingWord + sentence.charAt(i);
-  }
-
-  return reversingWord;
+  return sentence
+    .split(" ")
+    .map((v) => {
+      return v === " " ? " " : v.split("").reverse().join("");
+    })
+    .join(" ");
 }
 
 const sampleInput = {
@@ -15,22 +14,9 @@ const sampleInput = {
   input4: "",
   input5: "OneWord",
 };
-const input6 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const data = String(sampleInput.input3).split("");
-const originalData = String(sampleInput.input3).split("");
-console.log("check: -", input6.splice(1));
 
-const output = [];
-data.forEach((v, i) => {
-  if (v == " ") output.push(v);
-  if (v != " ") {
-  }
-});
-
-// split " " and then push " ", reverse word
-
-// console.log(
-//   Object.keys(sampleInput).map((k) => {
-//     return { [`${k} - ${sampleInput[k]}`]: reverseWords(sampleInput[k]) };
-//   })
-// );
+console.log(
+  Object.keys(sampleInput).map((k) => {
+    return { [`${k} - ${sampleInput[k]}`]: reverseWords(sampleInput[k]) };
+  })
+);
